472be27c50b31316eb529fec59d1d17b92c6e800
Shun <saitoshin2145@gmail.com>
2024-12-24 13:55:14 +0900

    Update server.py

--- server.py
+++ server.py
@@ -8,5 +8,8 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/demo")
+async def demo(name: str):
+    return {'Hello ' + name + ''}
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
